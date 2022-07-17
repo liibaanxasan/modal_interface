cb7c0d82809de206a385c03a79b03932aa8fe7a8
liibaan9911 <farhiyacabdi6@gmail.com>
2022-07-17 01:20:06 -0700

    modal interface

---
+++ Modal interface/script.js
@@ -0,0 +1,32 @@
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const btnCloseModal = document.querySelector(".close-modal");
+const btnsOpenModal = document.querySelectorAll(".show-modal");
+
+ for(let i=0; i < btnsOpenModal.length; i++){
+ 	console.log(btnsOpenModal[i])
+ 	btnsOpenModal[i].addEventListener('click', function(){
+ 		modal.classList.remove('hidden');
+ 		overlay.classList.remove('hidden');
+ 	});
+ }
+ btnCloseModal.addEventListener('click',function(){
+ 	modal.classList.add('hidden');
+ 		overlay.classList.add('hidden');
+
+ })
+
+overlay.addEventListener('click',function(){
+ 	modal.classList.add('hidden');
+ 		overlay.classList.add('hidden');
+
+ })
+document.addEventListener('keydown', function(e){
+    if(e.key === 'Escape'){
+        if(!modal.classList.contains('hidden')){
+            modal.classList.add('hidden');
+           overlay.classList.add('hidden');
+            
+        }
+    }
+})
